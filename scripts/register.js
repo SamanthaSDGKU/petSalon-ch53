d5f22f38694511161ec71dce449304c2b4c6622c
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2024-11-16 12:46:41 -0800

    finishin the object literal version

--- scripts/register.js
+++ scripts/register.js
@@ -12,19 +12,39 @@ let petSalon = {
 let pet1={
     name:"Scooby",
     age:80,
-    gender:"Male"
+    gender:"Male",
+    breed:"Dane",
+    service:"Grooming"
 }
 let pet2={
     name:"Scrappy",
     age:70,
-    gander:"Male"
+    gender:"Male",
+    breed:"Mixed",
+    service:"Vaccines"
 }
+let pet3={
+    name:"Speedy",
+    age:80,
+    gender:"Male",
+    breed:"Dane",
+    service:"Grooming"
+}
+let pet4={
+    name:"Tweety",
+    age:80,
+    gender:"Male",
+    breed:"Dane",
+    service:"Grooming"
+}
+
 //push the obj into the array
-pets.push(pet1,pet2);
+pets.push(pet1,pet2,pet3,pet4);
 
 function displayPetNames(){
-    console.log(pets[0].name);
-    console.log(pets[1].name);
-    console.log(" We have: " + pets.length + " pets");
+    for(let i=0;i<4;i++){
+        document.getElementById("petList").innerHTML+=`<li> ${pets[i].name} </li>`;
+    }
+    document.getElementById("petList").innerHTML+= "We have: " + pets.length + " pets";
 }
 
